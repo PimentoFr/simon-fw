7982ec6ef5d30a24569372d096b8686a7d419a56
Pimento <pimento.stream@gmail.com>
2020-09-22 21:31:03 +0200

    refactor: add event management to button controller
    Add some button events :
    - BUTTON_NO_PRESS : The button is not pressed
    - BUTTON_BEFORE_SHORT_PRESS: The button is pressed but the press duration is not enough to be handling as a SHORT_PRESS (less than 25ms)
    - BUTTON_SHORT_PRESS: The button is pressed between 25 ms and 1s.
    - BUTTON_SHORT_PRESS_AND_RELEASE : The button is released after a BUTTON_SHORT_PRESS event
    - BUTTON_LONG_PRESS: The button is pressed over than 1s.
    - BUTTON_LONG_PRESS_AND_RELEASE : The button is pressed over than 1s and released.

--- Core/Inc/button.h
+++ Core/Inc/button.h
@@ -18,9 +18,28 @@ typedef enum {
 	BUTTON_BLUE,
 	BUTTON_CONF_UP,
 	BUTTON_CONF_DOWN,
-	BUTTON_CONF_MENU
-} button_t;
+	BUTTON_CONF_MENU,
+	BUTTON_MAX_SIZE
+} buttonType_t;
 
-GPIO_PinState button_read(button_t button);
+typedef enum {
+	BUTTON_NO_PRESS = 0,
+	BUTTON_BEFORE_SHORT_PRESS, 			/* Pressed during less than 100 ms */
+	BUTTON_SHORT_PRESS,		 			/* Pressed between 100 ms and 1s */
+	BUTTON_SHORT_PRESS_AND_RELEASED, 	/* Button is released while it is in short press */
+	BUTTON_LONG_PRESS,					/* Pressed over than 1s */
+	BUTTON_LONG_PRESS_AND_RELEASED	 	/* Button is released while it is in long press */
+} buttonEvents_t;
+
+typedef struct {
+	buttonType_t buttonType;
+	buttonEvents_t currentEvent;	/* Current event */
+	uint32_t startPressTimestamp;	/* Start time of the button press */
+} buttonHandler_t;
+
+GPIO_PinState button_read(buttonType_t button);
+void button_init(void);
+void button_compute(void);
+buttonEvents_t button_getCurrentState(buttonType_t buttonType);
 
 #endif /* INC_BUTTON_H_ */
--- Core/Src/button.c
+++ Core/Src/button.c
@@ -9,11 +9,132 @@
 #include "button.h"
 
 /**
- * @brief Read the state of the button
+ * @brief Duration for a short and long press in milliseconds
+ */
+#define BUTTON_SHORT_PRESS_DURATION_IN_MS	(25)
+#define BUTTON_LONG_PRESS_DURATION_IN_MS	(1000)
+
+typedef struct {
+	uint32_t currentTimestamp;
+} buttonController_t;
+
+static buttonHandler_t _buttonHandlers[] = {
+		{.buttonType = BUTTON_RED, .currentEvent=BUTTON_NO_PRESS, .startPressTimestamp = 0},		/* BUTTON_RED */
+		{.buttonType = BUTTON_GREEN, .currentEvent=BUTTON_NO_PRESS, .startPressTimestamp = 0},		/* BUTTON_GREEN */
+		{.buttonType = BUTTON_YELLOW, .currentEvent=BUTTON_NO_PRESS, .startPressTimestamp = 0},		/* BUTTON_YELLOW */
+		{.buttonType = BUTTON_BLUE, .currentEvent=BUTTON_NO_PRESS, .startPressTimestamp = 0},		/* BUTTON_BLUE */
+		{.buttonType = BUTTON_CONF_UP, .currentEvent=BUTTON_NO_PRESS, .startPressTimestamp = 0},	/* BUTTON_CONF_UP */
+		{.buttonType = BUTTON_CONF_DOWN, .currentEvent=BUTTON_NO_PRESS, .startPressTimestamp = 0},	/* BUTTON_CONF_DOWN */
+		{.buttonType = BUTTON_CONF_MENU, .currentEvent=BUTTON_NO_PRESS, .startPressTimestamp = 0}	/* BUTTON_CONF_MENU */
+};
+
+static buttonController_t _buttonController = {
+		.currentTimestamp = 0
+};
+
+static buttonHandler_t * _button_getHandler(buttonType_t buttonType);
+static void _button_computeButton(buttonType_t buttonType);
+
+/**
+ * @brief Initialize the button controller
+ */
+void button_init(void) {
+
+	return;
+}
+
+/**
+ * @brief Compute the button controller
+ * 		  All buttons will be listening and their current state will be updated
+ */
+void button_compute(void) {
+	_buttonController.currentTimestamp = HAL_GetTick();
+
+	for(uint8_t index = 0; index < BUTTON_MAX_SIZE; index++) {
+		_button_computeButton((buttonType_t) index);
+	}
+}
+
+/**
+ * @brief Update the current event of the button according to their state
+ * @param buttonType Type of button to update
+ */
+static void _button_computeButton(buttonType_t buttonType) {
+	buttonHandler_t * buttonHandler = _button_getHandler(buttonType);
+	uint8_t state = button_read(buttonType);
+
+	if(buttonHandler == NULL) {
+		assert_param(!"Pointer NULL");
+		return;
+	}
+
+	switch(buttonHandler->currentEvent) {
+	case BUTTON_NO_PRESS:
+		if(state == GPIO_PIN_SET) {
+			buttonHandler->currentEvent = BUTTON_BEFORE_SHORT_PRESS;
+			buttonHandler->startPressTimestamp = _buttonController.currentTimestamp;
+		}
+		break;
+	case BUTTON_BEFORE_SHORT_PRESS:
+		if(state == GPIO_PIN_SET) {
+			if((_buttonController.currentTimestamp - buttonHandler->startPressTimestamp) >= BUTTON_SHORT_PRESS_DURATION_IN_MS) {
+				buttonHandler->currentEvent = BUTTON_SHORT_PRESS;
+			}
+		} else {
+			buttonHandler->currentEvent = BUTTON_NO_PRESS;
+		}
+		break;
+	case BUTTON_SHORT_PRESS:
+		if(state == GPIO_PIN_SET) {
+			if((_buttonController.currentTimestamp - buttonHandler->startPressTimestamp) >= BUTTON_LONG_PRESS_DURATION_IN_MS) {
+				buttonHandler->currentEvent = BUTTON_LONG_PRESS;
+			}
+		} else {
+			buttonHandler->currentEvent = BUTTON_SHORT_PRESS_AND_RELEASED;
+		}
+		break;
+	case BUTTON_LONG_PRESS:
+		if(state == GPIO_PIN_RESET) {
+			buttonHandler->currentEvent = BUTTON_LONG_PRESS_AND_RELEASED;
+		}
+		break;
+	case BUTTON_SHORT_PRESS_AND_RELEASED:
+	case BUTTON_LONG_PRESS_AND_RELEASED:
+		buttonHandler->currentEvent = BUTTON_NO_PRESS;
+		break;
+	default:
+		assert_param(!"Bad Value");
+		return;
+	}
+}
+
+
+/**
+ * @brief get the button corresponding Handler
+ * @param buttonType Type of button
+ * @return ButtonHandler* Pointer of the corresponding handler.
+ */
+static buttonHandler_t * _button_getHandler(buttonType_t buttonType) {
+	buttonHandler_t * result = NULL;
+
+	if((uint8_t) buttonType >= BUTTON_MAX_SIZE) {
+		assert_param(!"Bad value");
+		return NULL;
+	}
+
+	result = &_buttonHandlers[(uint8_t) buttonType];
+
+	return result;
+}
+
+/**
+ * @brief Read the state of the button.
  * @param button Button name
  * @return GPIO_PinState Button state
+ * @retval GPIO_PIN_SET The button is pressed
+ * @retval GPIO_PIN_RESET The button is not pressed
  */
-GPIO_PinState button_read(button_t button) {
+GPIO_PinState button_read(buttonType_t button) {
 
 	GPIO_TypeDef *GPIOx = NULL;
 	uint16_t GPIO_Pin = 0;
@@ -53,4 +174,21 @@ GPIO_PinState button_read(button_t button) {
 	}
 
 	return HAL_GPIO_ReadPin(GPIOx, GPIO_Pin) ? GPIO_PIN_RESET : GPIO_PIN_SET;
+}
+
+
+/**
+ * @brief Get the current state of the given button
+ * @param buttonType Type of button
+ * @return Current event fot the given button
+ */
+buttonEvents_t button_getCurrentState(buttonType_t buttonType) {
+
+	buttonHandler_t * buttonHandler = _button_getHandler(buttonType);
+	if(buttonHandler == NULL) {
+		assert_param(!"Pointer NULL");
+		return BUTTON_NO_PRESS;
+	}
+
+	return buttonHandler->currentEvent;
 }
